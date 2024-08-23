77b2223b0c9d6dc09899a0c4047ea69883a9d124
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-24 01:07:14 +0600

    Создана структура по работе с глобальным стейтом

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,14 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import {ToastContainer} from 'react-toastify';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastContainer position='bottom-right' />
-    <App />
+    <Provider store={store}>
+      <ToastContainer position='bottom-right' />
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
